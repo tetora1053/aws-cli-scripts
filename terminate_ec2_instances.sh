682519882e4cc795b03a77cbf8347132a7134f9b
yusuke shinoda <mofcarro1799@gmail.com>
2021-12-08 06:05:24 +0900

    terminate multiple instances

--- terminate_ec2_instances.sh
+++ terminate_ec2_instances.sh
@@ -3,6 +3,10 @@ INSTANCE_IDS=$(aws ec2 describe-tags --filters \
     "Name=resource-type,Values=instance" \
     "Name=value,Values=${TARGET_INSTANCE_NAME}" --output json | jq -r '.Tags[].ResourceId'
 )
-# TODO:複数対応
-# aws ec2 terminate-instances --instance-ids "id-xxxxxxxx" "id-yyyyyyyy"
-aws ec2 terminate-instances --instance-ids "${INSTANCE_IDS}"
+STR=""
+for INSTANCE_ID in ${INSTANCE_IDS}
+do
+    STR+="${INSTANCE_ID} "
+done
+echo ${STR}
+aws ec2 terminate-instances --instance-ids ${STR}
