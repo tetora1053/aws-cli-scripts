b70ea762e1be8ac3e145f3691f88cdbbca134783
Yusuke Shinoda <yusuke.shinoda@shelfy.jp>
2022-11-24 02:31:07 +0900

    Create and associate SG which allows specified IP to access to specified EC2 instance

---
+++ ops/allow_ip_tmporary_access_to_ec2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Create and associate SG which allows specified IP to access to specified EC2 instance
+# Delete the SG after 1 hour automatically
+#
+
+VPC_ID=$(aws ec2 describe-vpcs \
+  --filters Name=tag:Name,Values=BaseVPC/VPC \
+  --query 'Vpcs[].VpcId' \
+  --output text
+)
+
+# create SecurityGroup
+SG_ID=$(
+  aws ec2 create-security-group \
+    --group-name will-be-deleted-automatically \
+    --description tmp \
+    --vpc-i ${VPC_ID} \
+    --query 'GroupId' \
+    --output text
+)
+
+# ingress rules for specified IP
+aws ec2 authorize-security-group-ingress \
+  --group-id ${SG_ID} \
+  --protocol tcp \
+  --port 80 \
+  --cidr ${IP_ADDR}/32
+
+aws ec2 authorize-security-group-ingress \
+  --group-id ${SG_ID} \
+  --protocol tcp \
+  --port 443 \
+  --cidr ${IP_ADDR}/32
+
+INSTANCE_ID=$(
+  aws ec2 describe-instances \
+    --filters Name=tag:Name,Values=${INSTANCE_NAME} \
+    --query 'Reservations[].Instances[].InstanceId' \
+    --output text
+)
+
+# get current SecurityGroups to add another one
+CURRENT_SG_IDS=$(
+  aws ec2 describe-instance-attribute \
+    --instance-id ${INSTANCE_ID} \
+    --attribute groupSet --query 'Groups[].GroupId' \
+    --output text
+)
+
+# add SecurityGroup
+aws ec2 modify-instance-attribute \
+  --instance-id ${INSTANCE_ID} \
+  --groups ${CURRENT_SG_IDS} ${SG_ID}
+
+# TODO: delete sg automatically
+
